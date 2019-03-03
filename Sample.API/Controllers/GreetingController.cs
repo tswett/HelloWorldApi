9d07861430b67efe68f1b3e484724cb1e4fdca4a
Tanner Swett <tannerswett@gmail.com>
2019-03-03 13:51:19 -0500

    Initial implementation of Sample.API

---
+++ Sample.API/Controllers/GreetingController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Domain.Entities;
+using Sample.Domain.Abstract;
+
+namespace Sample.API.Controllers
+{
+    [Route("api/greeting")]
+    [ApiController]
+    public class GreetingController : ControllerBase
+    {
+        IGreetingService GreetingService { get; set; }
+
+        /// <summary>
+        /// Create a new <c>GreetingController</c> which uses the given <c>IGreetingService</c>.
+        /// </summary>
+        /// <param name="greetingService"></param>
+        public GreetingController(IGreetingService greetingService)
+        {
+            GreetingService = greetingService;
+        }
+
+        // GET api/greeting
+        [HttpGet]
+        public async Task<ActionResult<Greeting>> Get()
+        {
+            Greeting result = await GreetingService.GetGreetingAsync();
+            return result;
+        }
+    }
+}
--- Sample.API/Startup.cs
+++ Sample.API/Startup.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Sample.Domain.Abstract;
+using Sample.Infrastructure;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using SimpleInjector.Lifestyles;
 
 namespace Sample.API
 {
     public class Startup
     {
+        Container SIContainer = new Container();
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,11 +29,33 @@ namespace Sample.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            IntegrateSimpleInjector(services);
+        }
+
+        void IntegrateSimpleInjector(IServiceCollection services)
+        {
+            // Boilerplate taken from
+            // https://simpleinjector.readthedocs.io/en/latest/aspnetintegration.html
+
+            SIContainer.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddSingleton<IControllerActivator>(
+                new SimpleInjectorControllerActivator(SIContainer));
+            services.AddSingleton<IViewComponentActivator>(
+                new SimpleInjectorViewComponentActivator(SIContainer));
+
+            services.EnableSimpleInjectorCrossWiring(SIContainer);
+            services.UseSimpleInjectorAspNetRequestScoping(SIContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            InitializeContainer(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -43,6 +68,22 @@ namespace Sample.API
 
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        void InitializeContainer(IApplicationBuilder app)
+        {
+            // Boilerplate taken from
+            // https://simpleinjector.readthedocs.io/en/latest/aspnetintegration.html
+
+            // Add application presentation components:
+            SIContainer.RegisterMvcControllers(app);
+            SIContainer.RegisterMvcViewComponents(app);
+
+            // Add application services:
+            SIContainer.Register<IGreetingService, GreetingService>(Lifestyle.Scoped);
+
+            // Allow Simple Injector to resolve services from ASP.NET Core.
+            SIContainer.AutoCrossWireAspNetComponents(app);
         }
     }
 }
