c1d62b77fc16f6629515eba467b39d4abc19d99e
Tanner Swett <tannerswett@gmail.com>
2019-03-03 12:45:04 -0500

    Initial implementation of Sample.Domain project

---
+++ Sample.Domain/Abstract/IGreetingService.cs
@@ -0,0 +1,16 @@
+﻿using Sample.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Sample.Domain.Abstract
+{
+    /// <summary>
+    /// A service which can provide a <c>Greeting</c> object.
+    /// </summary>
+    public interface IGreetingService
+    {
+        /// <summary>
+        /// Get the current greeting.
+        /// </summary>
+        Task<Greeting> GetGreetingAsync();
+    }
+}
---
+++ Sample.Domain/Entities/Greeting.cs
@@ -0,0 +1,21 @@
+﻿namespace Sample.Domain.Entities
+{
+    /// <summary>
+    /// A greeting, consisting of a piece of text.
+    /// </summary>
+    public class Greeting
+    {
+        /// <summary>
+        /// The text of the greeting.
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Create a new greeting with the specified text.
+        /// </summary>
+        public Greeting(string text)
+        {
+            Text = text;
+        }
+    }
+}
