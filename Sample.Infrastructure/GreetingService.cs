719fa4513cf5f9215ae53ef5b79ed4c30cf47667
Tanner Swett <tannerswett@gmail.com>
2019-03-03 12:50:24 -0500

    Initial implementation of Sample.Infrastructure class

---
+++ Sample.Infrastructure/GreetingService.cs
@@ -0,0 +1,17 @@
+﻿using Sample.Domain.Abstract;
+using Sample.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Sample.Infrastructure
+{
+    /// <summary>
+    /// A service which provides <c>Greeting</c>s that have the text "Hello World".
+    /// </summary>
+    public class GreetingService : IGreetingService
+    {
+        public async Task<Greeting> GetGreetingAsync()
+        {
+            return new Greeting("Hello World");
+        }
+    }
+}
